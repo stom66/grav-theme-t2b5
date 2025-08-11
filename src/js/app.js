c0be84c82cb0ebd5f7cfccfe7e610c5c96342a38
stom <tomsteventon@gmail.com>
2025-08-11 16:37:10 +0100

    feat: change FontAwesome to use Bootstrap CDN

--- src/js/app.js
+++ src/js/app.js
@@ -3,7 +3,7 @@ import { createPopper } from '@popperjs/core';  // local Popper
 import 'bootstrap';  // local Bootstrap JS which will use the local Popper internally
 
 // Include Font Awesome JS
-import '@fortawesome/fontawesome-free/js/all.min.js';
+//import '@fortawesome/fontawesome-free/js/all.min.js';
 
 window.onload = function () {
 	console.log("window.onload()");

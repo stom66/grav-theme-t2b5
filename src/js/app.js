1d77250ed818da7f271938dc5c1f78baa6f9a3e0
stom <tomsteventon@gmail.com>
2025-08-11 16:11:44 +0100

    feat: use local css/js

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,10 @@
-// Blank app.js file, is automatically included
-window.onload = function() {
-    console.log("window.onLoad()")
+// Import Popper + Bootstrap
+import { createPopper } from '@popperjs/core';  // local Popper
+import 'bootstrap';  // local Bootstrap JS which will use the local Popper internally
+
+// Include Font Awesome JS
+import '@fortawesome/fontawesome-free/js/all.min.js';
+
+window.onload = function () {
+	console.log("window.onload()");
 };
